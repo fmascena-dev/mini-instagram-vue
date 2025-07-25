43b899322c70b2bc4eae9705d1c828c7e591981f
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-25 01:11:45 -0300

    feat(login): create login page

---
+++ mini-instagram-vue/src/pages/Login.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <form>
+            <h2>Login</h2>
+            <input v-model="email" type="text" placeholder="Digite o seu email">
+            <input v-model="password" type="password" placeholder="Digite a sua senha">
+
+            <button type="submit">Entrar</button>
+
+            <p></p>
+        </form>
+    </div>
+
+</template>
