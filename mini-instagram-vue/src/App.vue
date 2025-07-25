e16555fab72417302c956c23e362edbd0b830d96
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-25 01:23:06 -0300

    feat(app): set up root component with router view

--- mini-instagram-vue/src/App.vue
+++ mini-instagram-vue/src/App.vue
@@ -2,11 +2,7 @@
 </script>
 
 <template>
-  <div
-    class="min-h-screen bg-gray-950 text-white flex items-center justify-center"
-  >
-    <h1 class="text-4xl font-bold">Mini-Instagram em Vue.js 💚</h1>
-  </div>
+  <router-view />
 </template>
 
 <style scoped>
