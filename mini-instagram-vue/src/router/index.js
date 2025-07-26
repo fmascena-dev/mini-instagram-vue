7aa8420d49d49fe8184567410596c5b8ca1f7658
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-26 20:03:42 -0300

    feat(router): add feed route and root path redirect

--- mini-instagram-vue/src/router/index.js
+++ mini-instagram-vue/src/router/index.js
@@ -1,9 +1,11 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Login from "../pages/Login.vue";
+import Feed from "../pages/Feed.vue";
 
 const routes = [
   { path: "/", redirect: "/login" },
   { path: "/login", component: Login },
+  { path: "/feed", component: Feed },
 ];
 
 const router = createRouter({
