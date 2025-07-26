74dcb0057cbce07adc563c1000273a7f8524f3ff
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-26 18:41:25 -0300

    feat(feed): implement Unsplash photo fetching with loading/error states

--- mini-instagram-vue/src/pages/Feed.vue
+++ mini-instagram-vue/src/pages/Feed.vue
@@ -1,9 +1,36 @@
 <script setup>
+import { onMounted, ref } from "vue";
 
+const photos = ref([]);
+const loading = ref(true);
+const error = ref("");
+
+const ACCESS_KEY = import.meta.env.VITE_UNSPLASH_KEY;
+
+onMounted(async () => {
+  try {
+    const response = await fetch(
+      `https://api.unsplash.com/photos/?client_id=${ACCESS_KEY}`
+    );
+    const data = await responde.json();
+    photos.value = data;
+  } catch (err) {
+    error.value = "Erro ao carregar as fotos 😢";
+  } finally {
+    loading.value = false;
+  }
+});
 </script>
 
 <template>
+  <div>
+    <h2>Feed de Fotos 📸</h2>
+
     <div>
-        <h2>Feed de Fotos 📸</h2>
+      <div>
+        <img src="" alt="" />
+        <div></div>
+      </div>
     </div>
+  </div>
 </template>
