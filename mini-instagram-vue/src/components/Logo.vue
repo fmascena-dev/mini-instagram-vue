1a235d6eda9c3601a4f8dbde0cd7e549dc3a4081
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-07-26 22:39:43 -0300

    feat(components): create reusable Logo component with animated gradient

---
+++ mini-instagram-vue/src/components/Logo.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1
+    class="text-5xl font-bold leading-[5rem] tracking-[-0.02em] bg-gradient-to-r from-[#f09433] via-[#dc2743] to-[#bc1888] bg-[length:200%_200%] bg-clip-text text-transparent animate-gradient"
+  >
+    InstaPocket
+  </h1>
+</template>
+
+<style>
+@keyframes gradient {
+  0% {
+    background-position: 0% 50%;
+  }
+  50% {
+    background-position: 100% 50%;
+  }
+  100% {
+    background-position: 0% 50%;
+  }
+}
+
+.animate-gradient {
+  animation: gradient 4s ease infinite;
+}
+</style>
