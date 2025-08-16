cab95c58f95a4c92e16ec1a27fc4f710394bbbc6
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-08-16 16:35:40 -0300

    feat: create page Favorites

--- mini-instagram-vue/src/pages/Favorites.vue
+++ mini-instagram-vue/src/pages/Favorites.vue
@@ -1,5 +1,29 @@
 <script setup></script>
 
 <template>
-    <h1>Favoritos ❤️</h1>
+  <div class="min-h-screen bg-gray-950 text-white p-12">
+    <div class="flex justify-between items-center mb-8">
+      <h1 class="text-3xl font-bold">Favoritos ❤️</h1>
+
+      <button
+        class="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-800 transition-colors hover:cursor-pointer duration-500"
+      >
+        Limpar todos
+      </button>
+    </div>
+
+    <!-- Favoritos vazio -->
+     <div>
+        <div class="text-xl mb-4">💔</div>
+        <h2>Nenhuma foto favoritada ainda</h2>
+        <p>Explore o Feed e clique no coração para adicionar fotos aos seus favoritos!</p>
+
+        <RouterLink>
+            Ir para o Feed
+        </RouterLink>
+
+     </div>
+
+     
+  </div>
 </template>
