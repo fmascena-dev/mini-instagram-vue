32238a104c3cb9a7507287c58880027ffefaa5f8
fmascena-dev <felipe.mascena.dev@gmail.com>
2025-08-16 21:10:02 -0300

    feat(header): make navigation links functional and refine styling

--- mini-instagram-vue/src/components/Header.vue
+++ mini-instagram-vue/src/components/Header.vue
@@ -3,12 +3,12 @@ import Logo from "./Logo.vue";
 </script>
 
 <template>
-  <header>
+  <header class="flex flex-col justify-between items-center px-8 lg:flex-row bg-gray-950 border-b border-gray-800 py-4">
     <Logo />
-    <nav>
-      <RouterLink to="/feed">FEED</RouterLink>
-      <RouterLink to="/favorites">FAVORITOS</RouterLink>
-      <RouterLink to="login">SAIR</RouterLink>
+    <nav  class="flex items-center gap-4 font-medium px-2 space-x-6 text-[#e1e1e1] text-xl">
+      <RouterLink to="/feed" class="relative group transition duration-500 font-bold hover:text-[#bc1888]">FEED</RouterLink>
+      <RouterLink to="/favorites" class="relative group transition duration-500 font-bold hover:text-[#bc1888]">FAVORITOS</RouterLink>
+      <RouterLink to="login" class="relative group transition duration-500 font-bold hover:text-[#bc1888]">SAIR</RouterLink>
     </nav>
   </header>
 </template>
